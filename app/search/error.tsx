aac0f5509b0d92ec75c5b89bd6c09a0c9c42be45
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-11-27 13:37:14 +0200

    handle possible errors

---
+++ app/search/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
+
+interface Props {
+  error: string;
+  reset: () => void;
+}
+
+const Error = ({ error, reset }: Props) => {
+  useEffect(() => {
+    console.log("Error: ", error);
+  }, [error]);
+  return (
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="text-3xl mb-4">Something went wrong</h1>
+      <button className="text-blue-500" onClick={() => reset()}>Try again</button>
+    </div>
+  );
+};
+
+export default Error;
--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -1,22 +1,37 @@
 import { SearchData } from "@/app/types";
+import Link from "next/link";
 
 interface Props {
   searchParams: {
-    searchTerm: string
-  }
+    searchTerm: string;
+  };
 }
 
 const WebSearchPage = async ({ searchParams }: Props) => {
   const responce = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
+
+  if (!responce.ok) {
+    throw new Error("Something went wrong");
+  }
   const data = await responce.json();
 
   const results: SearchData[] = await data.items;
 
-  return <div>{results && results.map(result => (
-    <h1>{result.title}</h1>
-  ))}</div>;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">Try searching for something else or go back to the homepage</p>
+        <Link href="/" className="text-blue-500">Home</Link>
+      </div>
+    );
+  }
+
+  return (
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 };
 
 export default WebSearchPage;
