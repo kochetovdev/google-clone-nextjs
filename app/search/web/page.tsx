25ed809e82e4f82b328c744cf588cdd984aef924
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-11-27 14:26:52 +0200

    create searchResult component

--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -1,5 +1,6 @@
 import { SearchData } from "@/app/types";
 import Link from "next/link";
+import WebSearchResults from "../_components/WebSearchResults";
 
 interface Props {
   searchParams: {
@@ -8,6 +9,7 @@ interface Props {
 }
 
 const WebSearchPage = async ({ searchParams }: Props) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
   const responce = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -23,14 +25,20 @@ const WebSearchPage = async ({ searchParams }: Props) => {
     return (
       <div className="flex flex-col justify-center items-center pt-10">
         <h1 className="text-3xl mb-4">No results found</h1>
-        <p className="text-lg">Try searching for something else or go back to the homepage</p>
-        <Link href="/" className="text-blue-500">Home</Link>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage
+        </p>
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
       </div>
     );
   }
 
   return (
-    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+    <div>
+      <WebSearchResults results={data} />
+    </div>
   );
 };
 
